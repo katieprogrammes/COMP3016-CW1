d709c86e869b717bfa538be94fcd740e196a883b
katieprogrammes <katiehayman98@gmail.com>
2025-10-28 05:02:05 +0000

    Creating Components

--- COMP3016-CW1/main.cpp
+++ COMP3016-CW1/main.cpp
@@ -1,1 +1,33 @@
-#include "main.h"
+#include "Game.h"
+
+Game* game = nullptr;
+
+int main(int argc, char* argv[])
+{
+	const int FPS = 60;
+	const int frameDelay = 1000 / FPS;
+
+	Uint32 frameStart;
+	int frameTime;
+	game = new Game();
+
+	game->init("Help! My Friend is in a Dungeon...", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 640, false);
+
+	while (game->running())
+	{
+		frameStart = SDL_GetTicks();
+		game->handleEvents();
+		game->update();
+		game->render();
+
+		frameTime = SDL_GetTicks() - frameStart;
+
+		if (frameDelay > frameTime)
+		{
+			SDL_Delay(frameDelay - frameTime);
+		}
+	};
+
+	game->clean();
+	return 0;
+}
